7da408bf543e015b56b3ccc434fde6d0b45f4f2e
bibekkakati <bibekkakati0@gmail.com>
2021-04-24 23:56:19 +0530

    Optimization

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let scrollButton = document.getElementById("scrollButton");
+let copyright = document.getElementById("copyright");
 scrollButton.onclick = function () {
 	window.scrollTo(0, window.innerHeight);
 };
+copyright.innerHTML += `${new Date().getFullYear()}`;
