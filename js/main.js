d19bbd517fd15775b175a21cc94b7b38fe0fe23b
bibekkakati <bibekkakati0@gmail.com>
2021-04-28 00:04:13 +0530

    Url replacement

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-let scrollButton = document.getElementById("scrollButton");
-let copyright = document.getElementById("copyright");
-scrollButton.onclick = function () {
-	window.scrollTo(0, window.innerHeight);
-};
-copyright.innerHTML += `${new Date().getFullYear()}`;
+if (window.location.pathname !== "/") {
+	window.location.replace("/");
+} else {
+	let scrollButton = document.getElementById("scrollButton");
+	let copyright = document.getElementById("copyright");
+	scrollButton.onclick = function () {
+		window.scrollTo(0, window.innerHeight);
+	};
+	copyright.innerHTML += `${new Date().getFullYear()}`;
+}
