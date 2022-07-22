fd8e24815cb73b6deb9fde8074ee1878086d6dee
Bibek <bibekkakati0@gmail.com>
2022-07-23 01:29:13 +0530

    Content update and dark mode enabled

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 if (window.location.pathname !== "/") {
-	window.location.replace("/");
+    window.location.replace("/");
 } else {
-	let scrollButton = document.getElementById("scrollButton");
-	let copyright = document.getElementById("copyright");
-	scrollButton.onclick = function () {
-		window.scrollTo(0, window.innerHeight);
-	};
-	copyright.innerHTML += `${new Date().getFullYear()}`;
+    let scrollButton = document.getElementById("scrollButton");
+    let copyright = document.getElementById("copyright");
+    scrollButton.onclick = function () {
+        window.scrollTo(0, window.innerHeight);
+    };
+    copyright.innerHTML += `${new Date().getFullYear()}`;
+}
+
+if (window.matchMedia) {
+    enableDarkTheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
+    window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", (event) => {
+            enableDarkTheme(event.matches);
+        });
+}
+
+function enableDarkTheme(enable = false) {
+    const body = document.body;
+    body.style.backgroundColor = enable ? "#101010" : "#ffffff";
+    body.style.color = enable ? "#f0f8ff" : "#101010";
 }
